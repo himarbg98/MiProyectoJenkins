285199c9ef533930bb9f3e50d2367611316d33f5
himarbg98 <163432611+himarbg98@users.noreply.github.com>
2025-05-18 18:07:21 +0100

    Update MavenFreeStyleTest.java

--- src/test/java/com/mycompany/mavenfreestyle/MavenFreeStyleTest.java
+++ src/test/java/com/mycompany/mavenfreestyle/MavenFreeStyleTest.java
@@ -1,48 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
- */
 package com.mycompany.mavenfreestyle;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
-/**
- *
- * @author olivert
- */
 public class MavenFreeStyleTest {
-    
-    public MavenFreeStyleTest() {
-    }
 
-    /**
-     * Test of main method, of class MavenFreeStyle.
-     */
     @Test
-    public void testMain() {
-        System.out.println("main");
-        String[] args = null;
-        MavenFreeStyle.main(args);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testFibonacci_0() {
+        MavenFreeStyle app = new MavenFreeStyle();
+        assertEquals(0, app.fibonacci(0));
     }
 
-    @Test 
-    public void testFibonacci(){
-        //App test = new App();
-        MavenFreeStyle instance = new MavenFreeStyle();
-	assertTrue(instance.fibonacci(1) == 1);
+    @Test
+    public void testFibonacci_1() {
+        MavenFreeStyle app = new MavenFreeStyle();
+        assertEquals(1, app.fibonacci(1));
     }
-    @Test	
-    public void testFibonacci2(){
-	//App test = new App();
-        MavenFreeStyle instance = new MavenFreeStyle();
-        assertTrue(instance.fibonacci(10) == 5);
+
+    @Test
+    public void testFibonacci_5() {
+        MavenFreeStyle app = new MavenFreeStyle();
+        assertEquals(5, app.fibonacci(5)); // fibonacci(5) = 5
     }
-    
 }
